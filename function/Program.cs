ba7551b92c0f94e5745f26b002c25e774e43c162
havishat <sundariha@gmail.com>
2019-01-18 14:41:57 -0800

    first commit

---
+++ function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace function
+{
+    // public delegate int SomeOperation(int i, int j);
+    class Program
+    {
+        // static int Sum(int x, int y){
+        //     return x + y;
+        // }
+
+        static bool IsUpperCase(string str){
+            return str.Equals(str.ToUpper());
+        }
+
+        static void Main(string[] args)
+        {
+            Predicate<string> IsUpper = IsUpperCase;
+
+            bool result = IsUpper("HELLOW!");
+            Console.WriteLine(result);
+            // SomeOperation add = Sum;
+            // int restult = add(10, 10);
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine(restult);
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int index = 1;
+            // while(index <= 5)
+            // {
+            //     Console.WriteLine(index);
+            //     index++;
+            // }
+            // Console.ReadLine();
+            Console.WriteLine("\nWhat is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
