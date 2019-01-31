871e7fed9d2dc963cae6d1f24adc7089fd6df7a3
havishat <sundariha@gmail.com>
2019-01-31 15:10:31 -0800

    added C#tutorial

---
+++ encapsulation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace encapsulation
+{
+    class Program
+    {
+        private String studentName;
+        private int studentAge;
+
+        public String Name {
+            get {
+                return studentName;
+            }
+            set {
+                studentName = value;
+            }
+        }
+
+        public int Age {
+            get {
+                return studentAge;
+            }
+            set {
+                studentAge = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            DemoEncap obj = new DemoEncap();
+
+            obj.Name = "Ankita";
+
+            obj.Age = 21;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name:" + obj.Name);
+            Console.WriteLine("Age:" + obj.Age);
+
+        }
+    }
+}
---
+++ inheritanceg2g/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace inheritanceg2g
+{
+
+    class GFG {
+        public string name;
+        public string subject;
+
+        public void readers(string name, string subject){
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My Favorite Subject is: " + subject);
+        }
+    }
+
+    class GeeksforGeeks : GFG {
+
+        public GeeksforGeeks(){
+            Console.WriteLine("GeeksforGeeks");
+        }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GeeksforGeeks g = new GeeksforGeeks();
+            g.readers("kirti", "C#");
+        }
+    }
+}
